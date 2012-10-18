10ad15a436cb3b90723f8c979d6b2429df0c6757
fnobi <shin@fnobi.com>
2012-10-18 11:13:05 +0900

    さらに仕様をかきかき。まだすこし納得いかない。

--- test/basic-test.js
+++ test/basic-test.js
@@ -2,7 +2,39 @@ var buster = require('buster'),
     wp     = require(__dirname + '/..');
 
 buster.testCase('basic', {
-	'select all': function () {
+	'open url': function () {
+		assert(
+			wp.compile('http://fnobi.com/')
+			==
+			'open "http://facebook.com/"'
+		);
+	},
+	'with browser': function () {
+		wp.defineBrowser('safari', ['s', 'safari']);
+		assert(
+			wp.compile('http://fnobi.com/', ['s'])
+			==
+			'open -a "safari" "http://facebook.com/"'
+		);
+	},
+	'with place': function () {
+		wp.definePlace('http://facebook.com/', {
+			key     : ['fb', 'facebook.com']
+		});
+
+		wp.compile('fb');
+
+		assert((
+			wp.compile('fb')
+			==
+			'open "http://facebook.com/"'
+		) && (
+			wp.compile('fb')
+			==
+			'open "http://facebook.com/"'
+		));
+	},
+	'with place and browser': function () {
 		// wp.defineEnv('mac');
 		wp.defineBrowser('safari', ['s', 'safari']);
 		wp.definePlace('http://facebook.com/', {
@@ -17,7 +49,7 @@ buster.testCase('basic', {
 		assert(
 			wp.compile('fb')
 			==
-			'open -a safari "http://facebook.com/"'
+			'open -a "safari" "http://facebook.com/"'
 		);
 	}
 });
