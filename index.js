edb7fb5e7b66c7597bee0673d6f6574d5e53ea69
fnobi <shin@fnobi.com>
2012-10-24 01:00:24 +0900

    オプションによるブラウザ振り分け、「http://」の省略可まで実装。

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
+var fs = require('fs');
+
 process.env.NODE_CONFIG_DIR = __dirname + '/config';
 
 var wp = {
 	Place   : require(__dirname + '/lib/Place'),
-	Browser : require(__dirname + '/lib/Browser')
+	Browser : require(__dirname + '/lib/Browser'),
+
+	loadBookmark : function (bookmark) {
+		bookmark = bookmark || {};
+
+		this.Place.bookmark = bookmark.places || {};
+		this.Browser.bookmark = bookmark.browsers || {};
+	},
+
+	saveBookmarkFile : function (filePath) {
+		var bookmark = {
+			places: this.Place.bookmark,
+			browsers: this.Browser.bookmark
+		};
+
+		fs.writeFileSync(filePath, 'utf8', JSON.stringify(bookmark));
+	},
+
+	loadBookmarkFile : function (filePath) {
+		var content = fs.readFileSync(filePath, 'utf8');
+		return this.loadBookmark(JSON.parse(content) || {});
+	}
 };
 
 module.exports = wp;
