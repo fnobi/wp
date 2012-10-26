26a18b0fd3f45099f492d10b0c10d1d54b9384ad
fnobi <shin@fnobi.com>
2012-10-26 20:41:10 +0900

    きがついたらいろいろ変えてた。

--- test/basic-test.js
+++ test/basic-test.js
@@ -3,9 +3,8 @@ var buster = require('buster'),
 
 buster.testCase('command to open url', {
 	'normally': function () {
-		assert(
-			new wp.Place('http://facebook.com/').commandToOpen()
-			==
+		assert.equals(
+			new wp.Place('http://facebook.com/').commandToOpen(),
 			'open "http://facebook.com/"'
 		);
 	},
@@ -13,9 +12,8 @@ buster.testCase('command to open url', {
 		var browser = new wp.Browser('safari');
 		browser.saveAs(['safari', 's']);
 
-		assert(
-			new wp.Place('http://fnobi.com/').commandToOpen(browser)
-			==
+		assert.equals(
+			new wp.Place('http://fnobi.com/').commandToOpen(browser),
 			'open -a "safari" "http://fnobi.com/"'
 		);
 	},
@@ -23,9 +21,8 @@ buster.testCase('command to open url', {
 		var place = new wp.Place('http://facebook.com/');
 		place.saveAs(['fb', 'facebook', 'facebook.com']);
 
-		assert(
-			wp.Place.find('fb').commandToOpen()
-			==
+		assert.equals(
+			wp.Place.find('fb').commandToOpen(),
 			'open "http://facebook.com/"'
 		);
 	},
@@ -37,9 +34,8 @@ buster.testCase('command to open url', {
 		place.setBrowser(wp.Browser.find('s'));
 		place.saveAs(['fb', 'facebook', 'facebook.com']);
 
-		assert(
-			wp.Place.find('fb').commandToOpen()
-			==
+		assert.equals(
+			wp.Place.find('fb').commandToOpen(),
 			'open -a "safari" "http://facebook.com/"'
 		);
 	}
